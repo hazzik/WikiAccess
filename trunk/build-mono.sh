9fb4b43af75b57ffab3b4416807e25ec4920f928
vasilievvv <vasilievvv@44129ab6-3c32-0410-b606-980ee20bd62a>
2007-11-05 06:07:26 +0000

    * Tested and fixed build-mono.sh

--- trunk/build-mono.sh
+++ trunk/build-mono.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 mkdir mono-out
 cd mono-out
-gmcs -reference:System.Web.dll -target:library -out:WikiAccess.dll -doc:WikiAccess.xml -nowarn:1591 -nowarn:1587 ..\*.cs ..\Wikimedia\*.cs
-cd ..\
+gmcs -reference:System.Web.dll -target:library -out:WikiAccess.dll -doc:WikiAccess.xml -nowarn:1591 -nowarn:1587 ../*.cs ../Wikimedia/*.cs
+cd ..
