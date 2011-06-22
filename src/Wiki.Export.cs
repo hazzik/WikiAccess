d759543d456554758ed3790c48e956beb910da46
michaelhauer <michaelhauer@44129ab6-3c32-0410-b606-980ee20bd62a>
2011-06-22 17:37:55 +0000

    added Export feature

---
+++ src/Wiki.Export.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace WikiTools.Access
+{
+	partial class Wiki
+	{
+		/// <summary>
+		/// Exports all pages from a given category in one xml file.
+		/// Works similiar to Special:Export
+		/// <remarks>
+		/// If you set <paramref name="plainXmlDump"/> to true and save the content to a file
+		/// this file can be easily imported in other wikis using Special:Import.
+		/// </remarks>
+		/// </summary>
+		/// <param name="categoryName">category name without namespace prefix</param>
+		/// <param name="plainXmlDump">if set to true the result is the same like Special:Export, otherwise the result is wrapped in the usual API block</param>
+		/// <returns></returns>
+		public string ExportPagesFromCategory(string categoryName, bool plainXmlDump = false)
+		{
+			// api.php?action=query&export&exportnowrap&generator=categorymembers&gcmtitle=category:XYZ
+			string page = "api.php?format=xml&action=query&export&generator=categorymembers&gcmtitle=" +
+				string.Format("{0}:{1}", ns.GetNamespaceByID(Namespaces.Category), HttpUtility.UrlEncode(categoryName));
+			if (plainXmlDump)
+				page += "&exportnowrap";
+			return ab.CreateGetQuery(page).DownloadText();
+		}
+
+		/// <summary>
+		/// Exports all given pages in one xml file.
+		/// Works similiar to Special:Export.
+		/// <remarks>
+		/// If you set <paramref name="plainXmlDump"/> to true and save the content to a file
+		/// this file can be easily imported in other wikis using Special:Import.
+		/// </remarks>
+		/// </summary>
+		/// <param name="pages">page names</param>
+		/// <param name="plainXmlDump">if set to true the result is the same like Special:Export, otherwise the result is wrapped in the usual API block</param>
+		/// <returns></returns>
+		public string ExportPages(IEnumerable<string> pages, bool plainXmlDump = false)
+		{
+			// api.php?action=query&export&exportnowrap&titles=article1|article2
+			string page = "api.php?format=xml&action=query&export&titles=" + string.Join("|", pages);
+			if (plainXmlDump)
+				page += "&exportnowrap";
+			return ab.CreateGetQuery(page).DownloadText();
+		}
+	}
+}
